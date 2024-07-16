6dc7fdb8b42bba7cb55a009592cd2033a4bf2df9
Rafael Silva <rafaelmike33@gmail.com>
2024-07-16 01:34:45 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
@@ -35,6 +34,13 @@ public class Program {
         Seller newSeller = new Seller(null, "Gleize", "gleize@gmail.com", new Date(), 4500.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
 
 
     }
